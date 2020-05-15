cbcffa29217718d13f1e58d99354db7b913df249
Elioenai Ferrari <elioenaiferrari@gmail.com>
2020-05-15 10:47:51 -0300

    update

--- lib/modules/app/app_module.dart
+++ lib/modules/app/app_module.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:help_enem/modules/app_widget.dart';
+import 'package:help_enem/modules/app/app_widget.dart';
 
 class AppModule extends MainModule {
   @override
